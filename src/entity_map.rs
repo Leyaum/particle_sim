9b2bc0ab6111d53cfbcb29f864d36317b4cf2877
lwalsh-eifu <lwalsh@eifu.com>
2024-06-07 16:59:42 -0400

    Fixed issues with entity map

--- src/entity_map.rs
+++ src/entity_map.rs
@@ -10,7 +10,7 @@ pub struct EntityMap {
 }
 
 impl EntityMap {
-    pub fn init(mut map_size: Vec2, container_size: f32) -> Self {
+    pub fn new(mut map_size: Vec2, container_size: f32) -> Self {
         if map_size.x < 0.0 {
             map_size.x = -map_size.x;
         } if map_size.y < 0.0 {
@@ -19,7 +19,12 @@ impl EntityMap {
 
         let cols = (map_size.x / container_size).ceil() as usize;
         let rows = (map_size.y / container_size).ceil() as usize;
-        let containers = Vec::<Vec<u32>>::with_capacity(rows*cols);
+        let mut containers = Vec::<Vec<u32>>::with_capacity(rows*cols);
+        for i in 0..rows*cols {
+            let container = Vec::<u32>::new();
+            containers.push(container);
+        }
+        println!("num containers: {}", containers.len());
 
         return EntityMap {
             map_size: map_size,
@@ -32,11 +37,23 @@ impl EntityMap {
 
     pub fn add_entity(&mut self, id: u32, pos: Vec2) {
         let container_index = self.pos_to_container_index(pos);
+        println!("entity index: {}", container_index);
         let container = &mut self.containers[container_index];
         container.push(id);
+
+        for i in (0..self.rows).rev() {
+            for j in 0..self.cols {
+                if i*self.cols + j == container_index {
+                    print!("X ");
+                } else {
+                    print!("0 ");
+                }
+            }
+            print!("\n");
+        }
     }
 
-    fn pos_to_container_index(&mut self, mut pos: Vec2) -> usize {
+    pub fn pos_to_container_index(&mut self, mut pos: Vec2) -> usize {
         pos.x += self.map_size.x / 2.0;
         pos.y += self.map_size.y / 2.0;
         if pos.x > self.map_size.x {
@@ -50,8 +67,8 @@ impl EntityMap {
             pos.y = 0.0;
         }
 
-        let r = (pos.x / self.container_size) as usize;
-        let c = (pos.y / self.container_size) as usize;
-        return (r-1)*self.cols + c;
+        let r = (pos.y / self.container_size) as usize;
+        let c = (pos.x / self.container_size) as usize;
+        return r*self.cols + c;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,13 @@ use crate::entity_map::EntityMap;
 
 fn main() {
     let mut world = ParticleWorld::default();
-    let map_size = Vec2::new(100.0,100.0);
+    let map_size = Vec2::new(150.0,100.0);
+
+    let mut map = EntityMap::new(map_size, 10.0);
+    let pos1 = Vec2::new(74.0,0.0);
+    map.add_entity(1, pos1);
+    //let pos2 = Vec2::new(-5., -5.);
+    //map.add_entity(2, pos2);
 
     App::new()
         .add_plugins(DefaultPlugins)
@@ -29,11 +35,11 @@ fn setup(
     commands.spawn(Camera2dBundle::default());
 
     let color = Color::rgb(1.0,1.0,1.0);
-    let shape = Mesh2dHandle(meshes.add(Circle{radius: 50.0}));
+    let shape = Mesh2dHandle(meshes.add(Circle{radius: 5.0}));
     let mesh = MaterialMesh2dBundle {
         mesh: shape,
         material: materials.add(color),
-        transform: Transform::from_xyz(0.0,0.0,0.0),
+        transform: Transform::from_xyz(-49.0,-49.0,0.0),
         ..default()
     };
     commands.spawn(mesh);
