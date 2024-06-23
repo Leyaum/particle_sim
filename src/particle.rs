c9a60367e2d86409c29a290962def8938b6490d7
Leyaum <lpw716@gmail.com>
2024-06-23 03:43:11 -0400

    Basic particle collision math

--- src/particle.rs
+++ src/particle.rs
@@ -22,11 +22,12 @@ impl Default for Particle {
 }
 
 impl Particle {
-    pub fn new(pos: Vec2, velocity: Vec2, acceleration: Vec2, radius: f32) -> Self {
+    pub fn new(pos: Vec2, mass: f32, velocity: Vec2, acceleration: Vec2, radius: f32) -> Self {
         let transform = Transform::from_xyz(pos.x, pos.y, 0.0);
         Self {
             transform: transform,
             rigidbody: RigidBody {
+                mass: mass,
                 velocity: velocity,
                 acceleration: acceleration,
             },
