71c777d46c07d24950485abc5d01023222943e8b
Leyaum <lpw716@gmail.com>
2024-06-27 15:36:46 -0400

    Finally working collision detection

--- src/particle.rs
+++ src/particle.rs
@@ -2,12 +2,13 @@ use std::default::Default;
 use bevy::{
     prelude::*,
 };
-use crate::physics::RigidBody;
+use crate::physics::{CircleCollider, RigidBody};
 
 #[derive(Bundle)]
 pub struct Particle {
     pub transform: Transform,
     pub rigidbody: RigidBody,
+    pub circle_collider: CircleCollider,
 }
 
 impl Default for Particle {
@@ -15,12 +16,13 @@ impl Default for Particle {
         Self {
             transform: Default::default(),
             rigidbody: Default::default(),
+            circle_collider: Default::default(),
         }
     }
 }
 
 impl Particle {
-    pub fn new(pos: Vec2, velocity: Vec2, acceleration: Vec2) -> Self {
+    pub fn new(pos: Vec2, velocity: Vec2, acceleration: Vec2, radius: f32) -> Self {
         let transform = Transform::from_xyz(pos.x, pos.y, 0.0);
         Self {
             transform: transform,
@@ -28,6 +30,9 @@ impl Particle {
                 velocity: velocity,
                 acceleration: acceleration,
             },
+            circle_collider: CircleCollider {
+                radius: radius,
+            }
         }
     }
 }
